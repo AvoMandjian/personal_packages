d3ce6bd3eb07cfaa06ded51228becef13e0a9be2
Avo_Mandjian <95854652+Avo-Mandjian@users.noreply.github.com>
2021-12-12 13:23:52 +0200

    ref

--- lib/util_functions.dart
+++ lib/util_functions.dart
@@ -33,7 +33,7 @@ class GetColor extends Color {
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll("#", "");
     if (hexColor.length == 6) {
-      hexColor = "FF" + hexColor;
+      hexColor = "0XFF" + hexColor;
     }
     return int.parse(hexColor, radix: 16);
   }
