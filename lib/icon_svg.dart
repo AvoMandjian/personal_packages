6e9812665ed2c8a8dcc50585dbdeede002d5c741
Avo_Mandjian <95854652+Avo-Mandjian@users.noreply.github.com>
2021-12-13 12:01:46 +0200

    refa

--- lib/icon_svg.dart
+++ lib/icon_svg.dart
@@ -1,15 +1,18 @@
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 class BuildSvgIcon extends StatelessWidget {
   final String svgAssetPath;
   final double width;
   final double height;
+  final Color? color;
   const BuildSvgIcon({
     Key? key,
     required this.svgAssetPath,
     required this.width,
     required this.height,
+    this.color,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -17,6 +20,7 @@ class BuildSvgIcon extends StatelessWidget {
       svgAssetPath,
       width: width.w,
       height: height.h,
+      color: color,
     );
   }
 }
