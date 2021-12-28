c46b9da3a2b9705df88d62bf1bd96dfd77196246
Avo_Mandjian <95854652+Avo-Mandjian@users.noreply.github.com>
2021-12-28 22:48:34 +0200

    “refactored”

--- lib/button.dart
+++ lib/button.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BuildButton extends StatelessWidget {
@@ -10,6 +9,7 @@ class BuildButton extends StatelessWidget {
   final double width;
   final int height;
   final void Function()? onTap;
+  final BorderRadius? borderRadius;
   final AlignmentGeometry? alignment;
   const BuildButton({
     Key? key,
@@ -19,6 +19,7 @@ class BuildButton extends StatelessWidget {
     this.width = double.infinity,
     this.height = 60,
     this.onTap,
+    this.borderRadius,
     this.alignment,
   }) : super(key: key);
 
@@ -26,23 +27,26 @@ class BuildButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Container(
-        width: width.w,
-        height: height.h,
-        color: color,
-        child: Align(
-          alignment: alignment ?? Alignment.center,
-          child: AutoSizeText(
-            buttonText,
-            style: textStyle ??
-                TextStyle(
-                  fontFamily: 'MontserratSemiBold',
-                  fontSize: 13.sp,
-                  letterSpacing: 1,
-                  color: Colors.white,
-                ),
-            minFontSize: 1,
-            maxLines: 1,
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: Container(
+          width: width.w,
+          height: height.h,
+          color: color,
+          child: Align(
+            alignment: alignment ?? Alignment.center,
+            child: AutoSizeText(
+              buttonText,
+              style: textStyle ??
+                  TextStyle(
+                    fontFamily: 'MontserratSemiBold',
+                    fontSize: 13.sp,
+                    letterSpacing: 1,
+                    color: Colors.white,
+                  ),
+              minFontSize: 1,
+              maxLines: 1,
+            ),
           ),
         ),
       ),
